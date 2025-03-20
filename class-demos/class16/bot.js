a94dba0bef234830874cce72c107ce9c087be70d
samheckle <samheckle@yahoo.com>
2025-03-20 13:56:47 -0400

    added mastodon demo

---
+++ class-demos/class16/bot.js
@@ -0,0 +1,32 @@
+// import the installed libraries
+require("dotenv").config()
+const m = require("masto")
+
+const masto = m.createRestAPIClient({
+    url: "https://networked-media.itp.io/",
+    accessToken: process.env.TOKEN
+})
+
+async function makeStatus(text){
+
+    const status = await masto.v1.statuses.create({
+        // the thing that will be posted
+        status: text,
+        // change the visibility for testing purposes
+        visibility: "private"
+    })
+
+    console.log(status.url)
+}
+
+// makeStatus("hi")
+
+setInterval ( () =>{
+    let emoji = ["😭", "🤩", "💖"]
+
+    let rand = Math.floor(Math.random() * emoji.length)
+
+    let post = emoji[rand]
+
+    makeStatus(post)
+}, 5000)
