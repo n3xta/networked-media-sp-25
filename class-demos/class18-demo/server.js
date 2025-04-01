9a327090b8a4cfc2a0b18976a4fb5c391b8c483b
samheckle <samheckle@yahoo.com>
2025-04-01 14:46:45 -0400

    added class 18 demo

---
+++ class-demos/class18-demo/server.js
@@ -0,0 +1,85 @@
+// how do we know this is a npm project?
+// A: package.json!
+
+// what command do we run to start an npm project?
+// A: npm init
+
+// what does the below chunk of code do?
+// A: imports libraries
+const express = require("express"); // imports express
+const multer = require("multer");   // imports multer -- handles file upload
+const bodyParser = require("body-parser");  // imports body parser -- allows us to have a body in server request
+const nedb = require("@seald-io/nedb")
+
+// translates bits and bytes (literal memory and data) to something readable by the server
+const urlEncodedParser = bodyParser.urlencoded({ extended: true }); 
+
+// what is app?
+// A: instance of express
+const app = express();
+
+// what is this configuring?
+// A: destination for where files should be uploaded
+const upload = multer({
+  dest: "public/uploads",
+});
+
+let database = new nedb({
+  filename: "database.txt",
+  autoload: true // want the database to automatically load when the code starts
+})
+
+// what do each of these statements do?
+app.use(express.static("public"));  // set the default folder for any static files such as assets, css, html
+app.use(urlEncodedParser);        // middleware to make sure the bits and bytes can be understood by the app
+app.set("view engine", "ejs"); // allows us to use ejs
+
+// what is this?
+// A: route that handles when the client makes a request to /
+app.get("/", (request, response) => {
+  // response.send("server working");
+
+  // what steps do we need in order to use a template ejs file?
+  // 
+  let query = {} // return everything in the db
+  database.find(query).exec( (err, data)=>{
+    response.render('index.ejs', {posts: data})
+  } )
+
+  // make sure to comment out the res.send() code above
+});
+
+app.post("/upload", upload.single("theimage"), (req, res)=>{
+  console.log(req.body)
+  console.log(req.file)
+
+  let currentDate = new Date()
+
+  let data = {
+    text: req.body.text,
+    date: currentDate.toLocaleString(),
+    timestamp: currentDate.getTime()
+  }
+
+  if(req.file){
+    data.image = "/uploads/" + req.file.filename
+  }
+
+  // first param is the data we are adding
+  // second param is the callback
+  database.insert(data, (err, newData)=>{
+    console.log(newData)
+    res.redirect("/")
+  })
+})
+
+// what does the number signify?
+// A: port number!
+// how do we access this on the web?
+// A: ip address:port ex. localhost:6001 
+app.listen(6001, () => {
+  console.log("server started on port 6001");
+});
+
+// secret comment for later in the demo:
+// @seald-io/nedb
--- starter/class18-starter/server.js
+++ starter/class18-starter/server.js
@@ -1,48 +1,46 @@
 // how do we know this is a npm project?
-// A: package.json!
+// A: 
 
 // what command do we run to start an npm project?
-// A: npm init
+// A: 
 
 // what does the below chunk of code do?
-// A: imports libraries
-const express = require("express"); // imports express
-const multer = require("multer");   // imports multer -- handles file upload
-const bodyParser = require("body-parser");  // imports body parser -- allows us to have a body in server request
+// A: 
+const express = require("express"); 
+const multer = require("multer");  
+const bodyParser = require("body-parser");  
 
-// translates bits and bytes (literal memory and data) to something readable by the server
+// 
 const urlEncodedParser = bodyParser.urlencoded({ extended: true }); 
 
 // what is app?
-// A: instance of express
+// A: 
 const app = express();
 
 // what is this configuring?
-// A: destination for where files should be uploaded
+// A: 
 const upload = multer({
   dest: "public/uploads",
 });
 
 // what do each of these statements do?
-app.use(express.static("public"));  // set the default folder for any static files such as assets, css, html
-app.use(urlEncodedParser);        // middleware to make sure the bits and bytes can be understood by the app
-app.set("view engine", "ejs"); // allows us to use ejs
+app.use(express.static("public"));  // 
+app.use(urlEncodedParser);        // 
+app.set("view engine", "ejs"); // 
 
 // what is this?
-// A: route that handles when the client makes a request to /
+// A: 
 app.get("/", (request, response) => {
-  // response.send("server working");
-
   // what steps do we need in order to use a template ejs file?
   // 
   response.render('index.ejs', {})
-  // make sure to comment out the res.send() code above
+  // 
 });
 
 // what does the number signify?
-// A: port number!
+// A: 
 // how do we access this on the web?
-// A: ip address:port ex. localhost:6001 
+// A: 
 app.listen(6001, () => {
   console.log("server started on port 6001");
 });
